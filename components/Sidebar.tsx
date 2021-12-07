d9ae9e300f89cad2a77f8669962222b949ebf258
Lachlan <lachlanterrey@yahoo.com>
2021-12-07 16:36:36 +1100

    Sidebar draft complete

---
+++ components/Sidebar.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import homeIcon from "../public/assets/home-icon.svg";
+import listIcon from "../public/assets/list-icon.svg";
+import settingsIcon from "../public/assets/settings-icon.svg";
+import Image from "next/image";
+
+const Sidebar = () => {
+  return (
+    <div className=" fixed top-0 left-0 h-screen w-30 m-0 flex flex-col bg-gray-200">
+      <div className="my-auto">
+        <h1 className="absolute top-5 left-10 text-4xl text-red-600 font-helloSister">
+          STONKSEARCH
+        </h1>
+        <div className="p-10">
+          <Image src={homeIcon} width={50} height={50} />
+        </div>
+        <div className="p-10">
+          <Image src={listIcon} width={50} height={50} />
+        </div>
+        <div className="p-10">
+          <Image src={settingsIcon} width={50} height={50} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
